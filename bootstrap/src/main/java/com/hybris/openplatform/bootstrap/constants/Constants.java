00c65083216103c5dc0fd1b8a5c8019a2240602d
Arkady <arkady.syamtomov@hybris.com>
2018-01-28 17:49:07 +0100

    Clean up

--- bootstrap/src/main/java/com/hybris/openplatform/bootstrap/constants/Constants.java
+++ bootstrap/src/main/java/com/hybris/openplatform/bootstrap/constants/Constants.java
@@ -1,17 +1,9 @@
-/*
- * [y] hybris Platform
- *
- * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
- *
- * This software is the confidential and proprietary information of SAP
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with SAP.
- */
 package com.hybris.openplatform.bootstrap.constants;
 
 public class Constants
 {
+	private Constants() {}
+	
 	public static final String DEFAULT_ENCODING = "utf-8";
 
 	public static final String HTTP_HEADER_CONTENT_TYPE = "content-type";
